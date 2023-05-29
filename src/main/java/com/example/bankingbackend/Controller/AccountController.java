1eabaf99a859e07f7b527bfe08608c9d3e0c0d3b
Your Name <you@example.com>
2023-05-29 17:09:43 +0530

    adding account in admin

---
+++ src/main/java/com/example/bankingbackend/Controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.example.bankingbackend.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bankingbackend.Entity.Accounts;
+import com.example.bankingbackend.Service.AccountService;
+import com.example.bankingbackend.Service.LoanService;
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/")
+public class AccountController {
+	
+	@Autowired
+	public AccountService accountService;
+
+	@GetMapping("api/admin/accounts")
+	public List<Accounts> getAllAccounts() {
+		return accountService.getAllAccounts();
+	}
+	//Adding Employee
+	@PostMapping("api/admin/addAccount")
+	public Accounts addAccount(@RequestBody Accounts account)  {
+		return accountService.addAccounts(account);
+		
+	}
+}
