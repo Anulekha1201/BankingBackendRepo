930c1937850273e7ae1ece61bb30d2c366527d92
Your Name <you@example.com>
2023-05-30 10:40:56 +0530

    updated

--- src/main/java/com/example/bankingbackend/Controller/AccountController.java
+++ src/main/java/com/example/bankingbackend/Controller/AccountController.java
@@ -1,11 +1,15 @@
 package com.example.bankingbackend.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.bankingbackend.Entity.Accounts;
 import com.example.bankingbackend.Service.AccountService;
 import com.example.bankingbackend.Service.LoanService;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/")
@@ -25,10 +30,30 @@ public class AccountController {
 	public List<Accounts> getAllAccounts() {
 		return accountService.getAllAccounts();
 	}
-	//Adding Employee
+	
 	@PostMapping("api/admin/addAccount")
 	public Accounts addAccount(@RequestBody Accounts account)  {
-		return accountService.addAccounts(account);
+		return accountService.addAccounts(account);	
+	}
+	
+	@PutMapping("api/admin/updateAccount/{id}")
+	public Accounts updateAccount(@PathVariable Long id,@RequestBody Accounts account)
+	{
+		return accountService.updateAccount(id,account);
+	}
+	
+	@GetMapping("api/admin/getAccountById/{id}")
+    public Optional<Accounts> getAccountById(long id)  {
+		
+		Optional<Accounts> account = accountService.getAccountById(id);
+		return account;
+    }	
+	
+	@DeleteMapping("api/admin/deleteAccount/{id}")
+	public void deleteAccountById(long id) {
+		accountService.deleteAccount(id);
 		
 	}
+    
+    
 }
