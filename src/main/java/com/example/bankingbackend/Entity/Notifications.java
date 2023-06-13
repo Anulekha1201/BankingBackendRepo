33938e645d75cc646f6844590061e0020f86aeab
Anulekha1201 <anulekhaachanta7@gmail.com>
2023-06-13 11:56:14 +0530

    changes completed..

--- src/main/java/com/example/bankingbackend/Entity/Notifications.java
+++ src/main/java/com/example/bankingbackend/Entity/Notifications.java
@@ -6,8 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity
+@Data
+@AllArgsConstructor
+
+@Builder
 @Table(name = "Notifications")
 public class Notifications {
 	@Id
---
+++ src/test/java/com/example/bankingbackend/Service/UserInfoServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.bankingbackend.Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserInfoServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
